6782d07a7f6935ccfba3376d4fbf19b9dbf92595
AkhilUddandam <60453741+AkhilUddandam@users.noreply.github.com>
2020-04-04 16:21:12 -0700

    Update Hello.java

--- Lab04/src/Hello.java
+++ Lab04/src/Hello.java
@@ -11,8 +11,8 @@ public class Hello {
 		System.out.println("456345634 - This line was added by Uriel Diaz.");
 		System.out.println("301732964 - This line was added by Akhil Uddandam.");
 		System.out.println("218975615 - This line was added by Steven Lum.");
-		System.out.println("SacStateID - This line was added by Justin Filimon.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("837487489 - This line was added by Justin Filimon.");
+		System.out.println("4328748239 - This line was added by Daniel Mason.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
