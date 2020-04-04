9457c415e9c4992eb39e3908fbbd1f74b02555c6
Admin <Admin@LAPTOP-FRLQTSF7>
2020-04-04 16:07:12 -0700

    added name

--- Lab04/src/Hello.java
+++ Lab04/src/Hello.java
@@ -11,16 +11,8 @@ public class Hello {
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("301732964 - This line was added by Akhil Uddandam.");
 		System.out.println("218975615 - This line was added by Steven Lum.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-<<<<<<< HEAD
-		System.out.println("SacStateID - This line was added by Uriel Diaz.");
 		System.out.println("SacStateID - This line was added by Justin Filimon.");
-=======
 		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
->>>>>>> branch 'master' of https://github.com/Alexander-Maxwell/CSC-131-Lab04-Github.git
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
